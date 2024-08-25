4ecfe3c7b6225a5421a6ab743150777a3b71346c
LongPV772003 <phamvanlong7720033@gmail.com>
2024-08-25 20:50:43 +0700

    test

---
+++ config.js
@@ -0,0 +1,19 @@
+const CONFIG = {
+    introTitle: 'Babe à!',
+    introDesc: `Trái đất vốn lạ thường
+    Mà sao em cứ đi nhầm đường
+    Lạc vào tim anh lẻ loi
+    Đằng sau chữ yêu đây là thương`,
+    btnIntro: 'hihi',
+    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
+    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
+    btnYes: 'You thích me rồi phải không <33',
+    btnNo: 'Không, Tôi không thích :3',
+    question:'Trên thế giới hơn 7 tỉ người mà sao you lại chọn me <3',
+    btnReply: 'Gửi những lời iu thương <3',
+    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
+    mess: 'Biết mà 🥰. Love you nhiều nhiều 😘😘',
+    messDesc: 'Tối mai 8h anh qua đón nhé công chúa.',
+    btnAccept: 'Okiiiii lun <3',
+    messLink: 'https://www.facebook.com/hi.long.nee'
+}
